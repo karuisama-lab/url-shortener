c1b3d9d5bb52ae92721b3d31a1b89c1d0391fa2c
Polina <karuisama@gmail.com>
2026-04-18 17:05:01 +0300

    commit

--- apigateway/internal/http/dto/aliasdto/aliasdto.go
+++ apigateway/internal/http/dto/aliasdto/aliasdto.go
@@ -1,19 +1,16 @@
 package aliasdto
 
-// тут сохраняем то, что приходит от клиента на сервер и наоборот
-
-URLSaveRequest struct {
-URL string `json:"url"`
+type URLSaveRequest struct {
+	URL string `json:"url"`
 }
 
 type URLSaveResponse struct {
 	Message string `json:"message"`
-	Alias string `json:"alias"`
+	Alias   string `json:"alias"`
 }
 
 type URLGetResponse struct {
 	Message string `json:"message"`
-	URL string `json:"url"`
-	Alias string `json:"alias"`
+	URL     string `json:"url"`
+	Alias   string `json:"alias"`
 }
-
--- apigateway/internal/http/handlers/aliashandlers/aliashandlers.go
+++ apigateway/internal/http/handlers/aliashandlers/aliashandlers.go
@@ -2,7 +2,6 @@ package aliashandlers
 
 import (
 	"github.com/gofiber/fiber/v2"
-	"log/slog"
 	"url-shortener/aliasservice/domain"
 	"url-shortener/apigateway/internal/http/dto/aliasdto"
 )
@@ -22,29 +21,19 @@ func (handler *AliasHandler) SaveURL(ctx *fiber.Ctx) error {
 	if err := ctx.BodyParser(&req); err != nil {
 		return err
 	}
-	if req.URL == {
-		return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "url is required",
-		})
-		alias, err := handler.Service.SaveURL(ctx.Context(), req.URL)
-		if err != nil {return err
-			resp := aliasdto.URLSaveResponse{
-				Message: "url saved",
-				Alias: alias,
-			}
-			return ctx.Status(fiber.StatusCreated).JSON(resp)
-		}
-
-
-func (handler *AliasHandler) GetURL(ctx *fiber.Ctx) error {
-	alias := ctx.Params("alias")
-	if alias == "" {
-		return ctx.Status(400).JSON(map[string]string{
-			"message": "alias is required",
+	if req.URL == "" {
+		return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "url is required",
 		})
 	}
-	return ctx.Status(200).JSON(aliasdto.URLGetResponse{
-		URL:     "https.example.com",
-		Message: "url is found",
+
+	alias, err := handler.Service.SaveURL(ctx.Context(), req.URL)
+	if err != nil {
+		return err
+	}
+	resp := aliasdto.URLSaveResponse{
+		Message: "url saved",
 		Alias:   alias,
-	})
+	}
+	return ctx.Status(fiber.StatusCreated).JSON(resp)
 }
